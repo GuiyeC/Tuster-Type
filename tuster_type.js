539f5aefc252e2008ecceaeafede86ff69bf09ee
GuiyeC <guiyec@gmail.com>
2017-05-31 13:36:14 +0200

    Falling meteorites

--- tuster_type.js
+++ tuster_type.js
@@ -15,46 +15,167 @@ window.addEventListener("load", function () {
         .enableSound();
     
     Q.input.keyboardControls({
-        ENTER: "start"
+        ENTER: "start",
+
+        Y: "y",
+        U: "u",
+        I: "i",
+        H: "h",
+        J: "j",
+        K: "k"
     });
 
     Q.animations("meteorite_anim", {
         move: { frames: [0, 1, 2], rate: 1 / 5 },
-        die: { frames: [3, 4, 5], rate: 1 / 4, loop: false, trigger: "dead" }
+        poof: { frames: [3, 4, 5], rate: 1 / 4, loop: false, trigger: "dead" }
     });
 
-    Q.scene("main_game", function (stage) {
+    Q.scene("mainStage", function (stage) {
         // Q.stageTMX("level.tmx", stage);
 
         Q.state.reset({ score: 0 });
 
-        stage.add("viewport");
-        // stage.centerOn(150, 380);
-        // stage.viewport.offsetX = -120;
-        // stage.viewport.offsetY = 160;
-
         // Q.stageScene('scoreUI');
-        // Q.stageScene('background');
-        Q.stageScene('infoUI');
+        Q.stageScene('background', 0);
+        Q.stageScene('game', 1);
+        Q.stageScene('infoUI', 2);
+    });
+
+    Q.scene("game", function (stage) {
+        Q.input.on("y", this, function () {
+            console.log("killing y");
+            var meteorites = Q("Meteorite", 1);
+            meteorites.each(function () {
+                if (this.p.meteorite_type == 'Y') {
+                    this.play('poof');
+                }
+            });
+        });
+        Q.input.on("u", this, function () {
+            console.log("killing u");
+            var meteorites = Q("Meteorite", 1);
+            meteorites.each(function () {
+                if (this.p.meteorite_type == 'U') {
+                    this.play('poof');
+                }
+            });
+        });
+        Q.input.on("i", this, function () {
+            var meteorites = Q("Meteorite", 1);
+            meteorites.each(function () {
+                if (this.p.meteorite_type == 'I') {
+                    this.play('poof');
+                }
+            });
+        });
+        Q.input.on("h", this, function () {
+            var meteorites = Q("Meteorite", 1);
+            meteorites.each(function () {
+                if (this.p.meteorite_type == 'H') {
+                    this.play('poof');
+                }
+            });
+        });
+        Q.input.on("j", this, function () {
+            var meteorites = Q("Meteorite", 1);
+            meteorites.each(function () {
+                if (this.p.meteorite_type == 'J') {
+                    this.play('poof');
+                }
+            });
+        });
+        Q.input.on("k", this, function () {
+            var meteorites = Q("Meteorite", 1);
+            meteorites.each(function () {
+                if (this.p.meteorite_type == 'K') {
+                    this.play('poof');
+                }
+            });
+        });
 
         // Create the player and add them to the stage
         // var player = stage.insert(new Q.Mario());
 
-        // Add in a couple of enemies
-        stage.insert(new Q.Meteorite({ x: 0, y: 50 }));
-        stage.insert(new Q.Meteorite({ x: 250, y: 250 }));
-        stage.insert(new Q.Meteorite({ x: 250, y: 500 }));
-        stage.insert(new Q.Meteorite({ x: 500, y: 500 }));
+        /**
+         * Returns a random number between min (inclusive) and max (exclusive)
+         */
+        function getRandomArbitrary(min, max) {
+            return Math.random() * (max - min) + min;
+        }
+
+        var time = 0;        
+        stage.on('step', function (dt) {
+            // Update the current time offset
+            time += dt * 1000;
+            if (time > 1500) {
+                time = 0;
+
+                // Add in a couple of enemies
+                stage.insert(new Q.Meteorite({ x: getRandomArbitrary(70, 500), y: -200, meteorite_type: 'Y' }));
+                stage.insert(new Q.Meteorite({ x: getRandomArbitrary(70, 500), y: -230, meteorite_type: 'U' }));
+                stage.insert(new Q.Meteorite({ x: getRandomArbitrary(70, 500), y: -300, meteorite_type: 'I' }));
+                stage.insert(new Q.Meteorite({ x: getRandomArbitrary(70, 500), y: -240, meteorite_type: 'J' }));
+            }
+        });
     });
 
+    Q.component("defaultEnemy", {
+        added: function () {
+            var entity = this.entity;
+            entity.play('move');
+            entity.on("dead", entity, "destroy");
+
+            // Listen for a sprite collision, if it's the player,
+            // end the game unless the enemy is hit on top
+            entity.on("bump.left,bump.right,bump.bottom", function (collision) {
+                if (collision.obj.isA("Mario")) {
+                    collision.obj.gameOver();
+                }
+            });
+
+            // If the enemy gets hit on the top, destroy it
+            // and give the user a "hop"
+            entity.on("bump.top", function (collision) {
+                if (collision.obj.isA("Mario")) {
+                    this.play('die');
+                    this.p.vx = 0;
+                    collision.obj.p.vy = -300;
+                    Q.audio.play("kill_enemy.ogg");
+                }
+            });
+        }
+    });     
 
     // ## Enemy Sprite
     // Create the Enemy class to add in some baddies
     Q.Sprite.extend("Meteorite", {
         init: function (p) {
+            var color = null;
+            switch (p.meteorite_type) {
+                case 'Y':
+                    color = "blueMeteorite";
+                    break;
+                case 'U':
+                    color = "orangeMeteorite";
+                    break;
+                case 'I':
+                    color = "greenMeteorite";
+                    break;
+                case 'H':
+                    color = "redMeteorite";
+                    break;
+                case 'J':
+                    color = "purpleMeteorite";
+                    break;
+                case 'K':
+                    color = "yellowMeteorite";
+                    break;
+            }
+
             this._super(p, {
                 sprite: "meteorite_anim",
-                sheet: 'blueMeteorite',
+                sheet: color,
+                gravity: 0.2,
                 type: Q.SPRITE_ENEMY,
                 collisionMask: Q.SPRITE_DEFAULT
             });
@@ -100,17 +221,6 @@ window.addEventListener("load", function () {
             asset: "info.png",
             x: Q.width / 2, y: Q.height / 2,
         }));
-    }, { stage: 1 });
-
-    // ## Enemy Sprite
-    // Create the Enemy class to add in some baddies
-    Q.Sprite.extend("Background", {
-        init: function (p) {
-            this._super(p, {
-                asset: "day_background.png",
-                x: Q.width / 2, y: Q.height / 2, 
-            });
-        }
     });
 
        
@@ -135,7 +245,7 @@ window.addEventListener("load", function () {
 
         // Q.loadTMX("level.tmx", function () {
             // Finally, call stageScene to run the game
-            Q.stageScene("main_game");
+            Q.stageScene("mainStage");
         // }); 
     });
 
